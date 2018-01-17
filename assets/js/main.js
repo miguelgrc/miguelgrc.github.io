c25de6303d87016366085848672ac9c63910d1e8
miguelgrc <miguelparo12@hotmail.com>
2018-01-17 18:23:41 +0100

    Testing particles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 jQuery(document).ready(function($) {
 
+	particlesJS.load('particles-js', './particles.json', function() {
+		console.log('callback - particles.js config loaded');
+		console.log(JSON.stringify(url));
+	});
+
 
     /*======= Skillset *=======*/
     
